21b0669b66a63ad9ec024e44a0cea4357171fc8a
DESKTOP-9FFSOO7\tjdwn <tjdwns9656@naver.com>
2022-11-28 07:20:15 +0900

    초기 커밋.

---
+++ CircleDrawer.h
@@ -0,0 +1,32 @@
+﻿
+// CircleDrawer.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CCircleDrawerApp:
+// 이 클래스의 구현에 대해서는 CircleDrawer.cpp을(를) 참조하세요.
+//
+
+class CCircleDrawerApp : public CWinApp
+{
+public:
+	CCircleDrawerApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCircleDrawerApp theApp;
